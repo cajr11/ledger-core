781ecb4414d9a594e8c767f0ade55fc90345b4e9
cajr11 <caondo@yahoo.co.uk>
2026-03-29 09:16:12 -0600

    refactor(users): use db transactions for rollback in case failures occur during account creation steps

---
+++ api/src/modules/users/users.service.ts
@@ -0,0 +1,62 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { LedgerService } from '../ledger/ledger.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { AccountType } from 'src/types';
+import convertCountryToLedger from 'src/common/helpers/convertCountryToLedger.helper';
+import convertCountryToCurrency from 'src/common/helpers/convertCountryToCurrency.helper';
+
+@Injectable()
+export class UsersService {
+  private readonly logger = new Logger(UsersService.name, { timestamp: true });
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly ledgerService: LedgerService,
+  ) {}
+
+  async createUser(dto: CreateUserDto) {
+    try {
+      const userWithAccount = await this.prismaService.$transaction(
+        async (tx) => {
+          const user = await tx.user.create({
+            data: {
+              email: dto.email,
+              fullName: dto.fullName,
+              phone: dto.phone,
+              country: dto.country,
+            },
+          });
+
+          const tbAccId = await this.ledgerService.createAccount({
+            ledger: convertCountryToLedger(user.country),
+            code: AccountType.USER_WALLET,
+          });
+
+          if (!tbAccId) {
+            throw new Error('Failed to create tiger beetle account');
+          }
+
+          const pgUserAcc = await tx.userAccount.create({
+            data: {
+              userId: user.id,
+              tigerBeetleAccountId: tbAccId as bigint,
+              currency: convertCountryToCurrency(user.country),
+              accountType: AccountType[AccountType.USER_WALLET],
+            },
+          });
+
+          return {
+            user,
+            userAccount: pgUserAcc,
+          };
+        },
+      );
+
+      return userWithAccount;
+    } catch (error) {
+      this.logger.error('Failed to create user:', error);
+
+      throw error;
+    }
+  }
+}
