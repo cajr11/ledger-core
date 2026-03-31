6b17784578393d707b3d9e77910baf2fd44a6c09
cajr11 <caondo@yahoo.co.uk>
2026-03-31 15:45:38 -0600

    fix: clean up provider and webhook module DI
    Use dynamic register() pattern properly in ProviderModule, export
    both PAYMENT_PROVIDER token and StubProviderService. Remove unused
    ProviderModule import from WebhooksModule.

--- api/src/modules/providers/provider.module.ts
+++ api/src/modules/providers/provider.module.ts
@@ -3,17 +3,17 @@ import { DynamicModule, Module } from '@nestjs/common';
 import { StubProviderService } from './stub-provider.service';
 import { PAYMENT_PROVIDER } from './payment-provider-adapter.interface';
 
-@Module({
-  imports: [HttpModule],
-  providers: [StubProviderService],
-})
+@Module({})
 export class ProviderModule {
   static register(): DynamicModule {
     return {
       module: ProviderModule,
       imports: [HttpModule],
-      providers: [{ provide: PAYMENT_PROVIDER, useClass: StubProviderService }],
-      exports: [PAYMENT_PROVIDER],
+      providers: [
+        StubProviderService,
+        { provide: PAYMENT_PROVIDER, useClass: StubProviderService },
+      ],
+      exports: [PAYMENT_PROVIDER, StubProviderService],
     };
   }
 }
--- api/src/modules/webhooks/webhooks.module.ts
+++ api/src/modules/webhooks/webhooks.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WebhooksController } from './webhooks.controller';
-import { ProviderModule } from '../providers/provider.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { WebhooksService } from './webhooks.service';
 
 @Module({
-  imports: [ProviderModule, PrismaModule],
+  imports: [PrismaModule],
   controllers: [WebhooksController],
   providers: [WebhooksService],
 })
